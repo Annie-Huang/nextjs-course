b9125caa16b8c0beec0fb090211c91a44ea2c581
Annie Huang <annie.hn@gmail.com>
2021-05-22 15:57:45 +1000

    Explain the different betweem <a> to <Link>

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,25 @@
 // our-domain.com/news
 
+import Link from 'next/link';
+
 const NewsPage = () => {
-  return <h1>The News Page</h1>;
+  // If you just use <a href='/news/nextjs-is-a-great-framework'>NextJS Is A Great Framework</a>
+  // Then when you click the link and then refresh the page, it will come back to news\index.js
+  // It means you don't have a single page app here. It means that we're always sending a new request
+  // to the backend to fetch a new html page whenever the user navigates around
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href='/news/nextis-is-a-great-framework'>
+            NextJS Is A Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
