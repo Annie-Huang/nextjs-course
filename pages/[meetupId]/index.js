bd2d8ec716adbb75f6bd9326292a13be3f5055ee
Annie Huang <annie.hn@gmail.com>
2021-05-23 20:31:12 +1000

    Use context.params in getStaticProps(context)

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -13,13 +13,26 @@ const MeetupDetails = () => {
 };
 
 export async function getStaticProps(context) {
+  /*
+  // Inside getStaticProps(), you cannot access useRouter
+  const router = useRouter();
+  console.log(router.query.newsId); // log what is the newsId value.
+  */
+
+  // When you use context in getServerSideProps(), you can access
+  //   const req = context.req;
+  //   const res = context.res;
+  // When you use context in getStaticProps(), you can access to context.params.
+  const meetupId = context.params.meetupId; // [meetupId]\index.js
+  console.log('meetupId=', meetupId);
+
   // fetch data from API
   return {
     props: {
       meetupData: {
         image:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-        id: 'm1',
+        id: meetupId,
         title: 'A First Meetup',
         address: 'Some Street 5, Some City',
         description: 'The meetup description',
