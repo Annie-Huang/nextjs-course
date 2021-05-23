6654d9acd32b556dbbfb4bac36f0627634b6884e
Annie Huang <annie.hn@gmail.com>
2021-05-23 22:16:20 +1000

    Add in pages/api/new-meetup.js for mongodb connection. Which will not work as my mongodb cluster is gone. I just coded along...

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -4,11 +4,26 @@
 
 // when calling /api/new-meetup, this function in this file which got the "export default" will be triggered:
 
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    // const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://annie:<password>@testcluster1-4fb1w.mongodb.net/test?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.insertOne(data);
+    console.log('result=', result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
 
