5b8b206ead861c7a5b30404f8faeb9de3256527e
Annie Huang <annie.hn@gmail.com>
2021-05-28 15:56:00 +1000

    Redirect to another location once we submit the page

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,11 @@
 // our-domain.com/new-meetup
 
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+import { useRouter } from 'next/router';
 
 const NewMeetupPage = () => {
+  const router = useRouter();
+
   const addMeetupHandler = async (enteredMeetupData) => {
     console.log('enteredMeetupData=', enteredMeetupData);
     // const response = await fetch('https://some-domain.com/abc');
@@ -17,6 +20,8 @@ const NewMeetupPage = () => {
     const data = await response.json();
 
     console.log(data);
+
+    router.push('/'); // if you don't want them to use back button you can do router.replace('XXX')
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
